e346cb0a17a3a872230a56713f74184972ad21f0
Canop <cano.petrole@gmail.com>
2017-04-09 16:05:45 +0200

    snow disabled on non fun browsers

--- plugin/client-scripts/snow-client.js
+++ plugin/client-scripts/snow-client.js
@@ -1,4 +1,4 @@
-miaou(function(gui, locals, plugins, ws){
+miaou(function(gui, locals, plugins, prefs, ws){
 
 	var	DONT_REPLAY = false,
 		timer,
@@ -7,6 +7,10 @@ miaou(function(gui, locals, plugins, ws){
 	plugins.snow = {
 		start: function(){
 			ws.on('snow.launch', function(snowfall){
+				if (prefs.funLowerThan("normal")) {
+					console.log("no fun, no snow");
+					return;
+				}
 				console.log('snowfall:', snowfall);
 				if (gui.mobile) {
 					if (snowfall.global) return;
